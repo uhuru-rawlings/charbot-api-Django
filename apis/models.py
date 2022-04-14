86dd7afe7181cb29cabd1435c45472707367a6cd
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-14 22:10:13 +0300

    added registration model

--- apis/models.py
+++ apis/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Regisreation(models.Model):
+    username = models.CharField(max_length=100)
+    phonenumber = models.CharField(max_length=13)
+    userimage = models.ImageField(upload_to = 'images/')
