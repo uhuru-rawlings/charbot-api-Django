7e3da116f01d558e81bad98e9c1a4701154f3ca6
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-14 22:52:16 +0300

    added import to view

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.hashers import make_password,check_password
+from .serializers import RegistrationSerializer,ContactsSerializer,ChartsSerializer
+from .models import Regisration,Contacts,Charts
 # Create your views here.
