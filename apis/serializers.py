fd5e2203efe5780fa5a6560f8e4513f591f67f78
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-14 22:31:03 +0300

    added registrationserializer

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Regisration,Contacts,Charts
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Regisration
+        fields = ['username','phonenumber','userimage','password','is_active','is_loggedin']
