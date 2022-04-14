d88f07082350d8e09fdb9cb31deb8da3e1c618c0
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-14 23:41:09 +0300

    added login db credentials to environment

--- chartapp/settings.py
+++ chartapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z&+&6ixqbf$p#w!js*ne_2jbm%3cg8q5$)@5l*zb!6g-=jp^yx'
+SECRET_KEY = config('SECRETE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
