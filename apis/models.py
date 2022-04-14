40894ea4c14f370c59261b249b57514da4db3fa4
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-14 22:16:24 +0300

    added contact models

--- apis/models.py
+++ apis/models.py
@@ -5,3 +5,20 @@ class Regisreation(models.Model):
     username = models.CharField(max_length=100)
     phonenumber = models.CharField(max_length=13)
     userimage = models.ImageField(upload_to = 'images/')
+    password = models.CharField(max_length=300)
+    is_active = models.BooleanField(default=True)
+    is_loggedin = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'Regisreation'
+
+    def __str__(self):
+        return self.username
+
+class Contacts(models.Model):
+    user = models.ForeignKey(to, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100)
+    phonenumber = models.CharField(max_length=13)
+
+    class Meta:
+        db_table = 'Contacts'
